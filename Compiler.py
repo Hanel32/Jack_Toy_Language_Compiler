3b41a1b83c3ee8076795d4d6a8926ba50a57908d
Carson Hanel <carsonh14@gmail.com>
2017-12-01 23:30:18 -0600

    Update Compiler.py

--- Compiler.py
+++ Compiler.py
@@ -11,14 +11,57 @@ class Compiler():
         self.compileClass()
         
     def compileClass():
+      #Example:
+      #    <class>
+      #    <keyword> class </keyword>
+      #    <identifier> Main </identifier>
+      #    <symbol> { </symbol>
+      #              <classVarDec>
+      #              <keyword> static </keyword>
+      #              <keyword> boolean </keyword>
+      #              <identifier> test </identifier>
+      #              <symbol> ; </symbol>
+      #             </classVarDec>
+      
         
-    def compileClassVarDec():
+    def compileClassVarDec():  
+      # Example:
+      #   <classVarDec>
+      #    <keyword> static </keyword>
+      #    <keyword> boolean </keyword>
+      #    <identifier> test </identifier>
+      #    <symbol> ; </symbol>
+      #   </classVarDec>
+
         
     def compileSubroutine():
-        
+      # Example:
+      #    <subroutineDec>
+      #    <keyword> function </keyword>
+      #    <keyword> void </keyword>
+      #    <identifier> main </identifier>
+      #   <symbol> ( </symbol>
+      #              <parameterList>
+      #              </parameterList>
+      #              <symbol> ) </symbol>
+      #             <subroutineBody>
+      #              <symbol> { </symbol>
+      #                        <varDec>
+      
     def compileParameterList():
+      #Example:
+      #  <parameterList>
+      #  </parameterList>
+
         
     def compileVarDec():
+      #Example:
+      #<varDec>
+      #  <keyword> var </keyword>
+      #  <identifier> SquareGame </identifier>
+      #  <identifier> game </identifier>
+      #  <symbol> ; </symbol>
+      #</varDec>
         
     def compileStatements():
         
