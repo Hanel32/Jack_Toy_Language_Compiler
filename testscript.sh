db9f5ea51d05f953714e3544fe872f54ab942d5c
Max <maxsloeffler@gmail.com>
2017-12-06 22:55:52 -0600

    added testscript

---
+++ testscript.sh
@@ -0,0 +1,16 @@
+python JackA*.py /mnt/c/Users/Max/Downloads/nand2tetris/projects/10/A*/Main.jack
+python JackA*.py /mnt/c/Users/Max/Downloads/nand2tetris/projects/10/E*/Main.jack
+python JackA*.py /mnt/c/Users/Max/Downloads/nand2tetris/projects/10/S*/Main.jack
+python JackA*.py /mnt/c/Users/Max/Downloads/nand2tetris/projects/10/E*/Square.jack
+python JackA*.py /mnt/c/Users/Max/Downloads/nand2tetris/projects/10/S*/Square.jack
+python JackA*.py /mnt/c/Users/Max/Downloads/nand2tetris/projects/10/E*/SquareGame.jack
+python JackA*.py /mnt/c/Users/Max/Downloads/nand2tetris/projects/10/S*/SquareGame.jack
+
+
+~/312/nand2tetris/tools/TextComparer.sh /mnt/c/Users/Max/Downloads/nand2tetris/p*/10/A*/MainP.xml /mnt/c/Users/Max/Downloads/nand2tetris/p*/10/A*/Main.xml
+~/312/nand2tetris/tools/TextComparer.sh /mnt/c/Users/Max/Downloads/nand2tetris/p*/10/E*/MainP.xml /mnt/c/Users/Max/Downloads/nand2tetris/p*/10/E*/Main.xml
+#~/312/nand2tetris/tools/TextComparer.sh /mnt/c/Users/Max/Downloads/nand2tetris/p*/10/S*/MainP.xml /mnt/c/Users/Max/Downloads/nand2tetris/p*/10/S*/Main.xml
+~/312/nand2tetris/tools/TextComparer.sh /mnt/c/Users/Max/Downloads/nand2tetris/p*/10/E*/SquareP.xml /mnt/c/Users/Max/Downloads/nand2tetris/p*/10/E*/Square.xml
+~/312/nand2tetris/tools/TextComparer.sh /mnt/c/Users/Max/Downloads/nand2tetris/p*/10/S*/SquareP.xml /mnt/c/Users/Max/Downloads/nand2tetris/p*/10/S*/Square.xml
+~/312/nand2tetris/tools/TextComparer.sh /mnt/c/Users/Max/Downloads/nand2tetris/p*/10/E*/SquareGameP.xml /mnt/c/Users/Max/Downloads/nand2tetris/p*/10/E*/SquareGame.xml
+~/312/nand2tetris/tools/TextComparer.sh /mnt/c/Users/Max/Downloads/nand2tetris/p*/10/S*/SquareGameP.xml /mnt/c/Users/Max/Downloads/nand2tetris/p*/10/S*/SquareGame.xml
